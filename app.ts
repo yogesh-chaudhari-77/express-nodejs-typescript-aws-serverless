c3354724b12429311fea9ad161e595ecf2628ac0
Yogeshwar Chaudhari <chaudhari.yogesh20@gmail.com>
2022-07-03 08:53:14 +0930

    added typescript

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
@@ -19,4 +19,4 @@ app.use((req, res, next) => {
   });
 });
 
-module.exports = app;
+export default app;
